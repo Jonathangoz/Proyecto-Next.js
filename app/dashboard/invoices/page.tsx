65a6fd3d9184e605cff5e20d30393ee9b6239fbd
Jonathangoz <jonathan942115@gmail.com>
2025-08-18 10:38:35 -0500

    metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from "react";
+import { Metadata } from 'next';
 import Pagination from "@/app/ui/invoices/pagination";
 import Table from "@/app/ui/invoices/table";
 import Search from "@/app/ui/search";
@@ -6,6 +7,10 @@ import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from "@/app/ui/Fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { fetchInvoicesPages } from "@/app/lib/data";
+
+export const metadata: Metadata = {
+    title: 'Facturación',
+}
 
 export default async function Page(props: {
     searchParams?: Promise<{
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
+import { Metadata } from 'next';
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/Fonts';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Dashboard',
+    default: 'Dashboard | Next.js App',
+  },
+  description: 'A simple dashboard built with Next.js',
+  metadataBase: new URL('https://nextjs-dashboard.vercel.app'),
+}
 
 export default function RootLayout({
   children,
